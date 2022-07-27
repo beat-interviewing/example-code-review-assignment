8c25997811ea8b8419e29701b914b545db8c578b
Alex Kalyvitis <a.kalyvitis@thebeat.co>
2022-07-27 12:46:40 +0200

    Log response status code

--- beatly/middleware.go
+++ beatly/middleware.go
@@ -11,16 +11,30 @@ func LoggingMiddleware(logger log.Logger) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			defer func(start time.Time) {
-				next.ServeHTTP(w, req)
+				iw := &interceptingWriter{w, http.StatusOK}
+				next.ServeHTTP(iw, req)
 				now := time.Now()
 				logger.Log(
-					"ts", now,
-					"d", now.Sub(start),
+					"time", now,
+					"duration", now.Sub(start),
 					"method", req.Method,
 					"path", req.URL.Path,
 					"ip", req.RemoteAddr,
+					"status", iw.code,
 				)
 			}(time.Now())
 		})
 	}
+}
+
+type interceptingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+// WriteHeader may not be explicitly called, so care must be taken to
+// initialize w.code to its default value of http.StatusOK.
+func (w *interceptingWriter) WriteHeader(code int) {
+	w.code = code
+	w.ResponseWriter.WriteHeader(code)
 }
